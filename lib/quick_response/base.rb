ec95815ab4fa1207e7be6ae7018d66618ff9419d
Robin Clart <robin@charlin.lu>
2011-03-02 18:08:25 +0100

    Refactoring

--- lib/quick_response/base.rb
+++ lib/quick_response/base.rb
@@ -3,24 +3,19 @@ module QuickResponse
     GOOGLE_CHART_API = "http://chart.apis.google.com/chart"
     DEFAULT_OPTIONS  = { :size => 256 }
 
+    attr_reader   :content
+    attr_accessor :size
+
     def initialize(*args)
       # Extract options
       options = args.last.is_a?(Hash) ? DEFAULT_OPTIONS.merge(args.pop) : DEFAULT_OPTIONS
 
-      @chl = to_format(args)
-      @chs = options[:size]
+      self.content = args
+      self.size    = options[:size]
     end
 
-    def content
-      @chl
-    end
-
-    def size
-      @chs
-    end
-    
-    def size=(size)
-      @chs = size
+    def content=(*args)
+      @content = to_format(args)
     end
 
     def image_url
@@ -49,14 +44,11 @@ module QuickResponse
 
     def self.format(format, options = {})
       define_method :to_format do |args|
-        args = args.slice(0, options[:limit]) if options[:limit]
-        str  = args.join(options[:join])
+        args   = args.slice(0, options[:limit]) if options[:limit]
+        output = options[:output] || format
+        str    = args.join(options[:join])
 
-        if str.match(Regexp.new(format))
-          str
-        else # If the argument doesn't match the format.
-          (options[:output] || format).gsub("(.*)", str)
-        end
+        str.match(format) ? str : output.gsub("(.*)", str)
       end
       private :to_format
     end
