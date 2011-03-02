6cd4cc185e90d44d5c5c182e57b8643eaff2e152
Robin Clart <robin@charlin.lu>
2011-03-02 23:41:11 +0100

    Escape maps arguments

--- lib/quick_response/types/maps.rb
+++ lib/quick_response/types/maps.rb
@@ -1,5 +1,9 @@
 module QuickResponse
   class Maps < ::QuickResponse::Base
     format "http://maps.google.com/maps?q=(.*)"
+
+    def initialize(*args)
+      super(*args.each { |a| CGI.escape(a) })
+    end
   end
 end
